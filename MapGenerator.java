ab8a37c307a954660e1e6186a9914925c011d53f
Aditya <adityapmodi@gmail.com>
2022-08-26 17:34:17 -0400

    test

--- MapGenerator.java
+++ MapGenerator.java
@@ -24,7 +24,7 @@ public class MapGenerator {
         for(int i = 0; i < map.length; i++) {
             for(int j = 0; j < map[i].length; j++) {
                if(map[i][j] > 0) {
-                    g.setColor(Color.white);
+                    g.setColor(Color.red);
                     g.fillRect(j * brickWidth + 80, i * brickHeight + 50, brickWidth, brickHeight);
 
                     g.setStroke(new BasicStroke(3));
